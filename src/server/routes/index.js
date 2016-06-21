e015e6d5576531d60530596e69c268bf11c1ba90
Danny Robinson <daniel@djrobinson.me>
2016-06-20 18:35:53 -0600

    removed nodemailer, sendgrid functional

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -6,21 +6,17 @@ var sg = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY)
 
 var pw = process.env.GMAIL_PW;
 
-var transporter = nodemailer.createTransport('smtps://dannyrobinsontestmail%40gmail.com:'+pw+'@smtp.gmail.com');
 var helper = require('sendgrid').mail;
-console.log("SENDGRID!", process.env.SENDGRID_API_KEY);
-
 
 router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../../client', 'index.html'));
 });
 
-router.post('/sendgrid', function(req, res, next){
-
-  from_email = new helper.Email("test@example.com")
-  to_email = new helper.Email("daniel@djrobinson.me")
-  subject = "Hello World from the SendGrid Node.js Library"
-  content = new helper.Content("text/plain", "some text here")
+router.post('/email', function(req, res, next){
+  from_email = new helper.Email(req.body.email);
+  to_email = new helper.Email("daniel@djrobinson.me");
+  subject = req.body.subject;
+  content = new helper.Content("text/plain", req.body.message);
   mail = new helper.Mail(from_email, subject, to_email, content)
 
   var requestBody = mail.toJSON()
@@ -32,25 +28,8 @@ router.post('/sendgrid', function(req, res, next){
     console.log(response.statusCode)
     console.log(response.body)
     console.log(response.headers)
-    res.json("worked?")
+    res.redirect('/');
   })
-})
-
-router.post('/email', function(req, res, next){
-  console.log(req.body);
-  var mailOptions = {
-      from: req.body.email,
-      to: 'daniel@djrobinson.me',
-      subject: req.body.subject,
-      text: req.body.message,
-  };
-  transporter.sendMail(mailOptions, function(error, info){
-      if(error){
-          return console.log(error);
-      }
-      console.log('Message sent: ' + info.response);
-      res.redirect('/');
-  });
 });
 
 router.get('/resume', function(req, res, next){
